a91c4e4ce7d9cb7dd3dc13cdd83c9c9325a6ae9b
Lucasgarcc <luuccasgarciia@gmail.com>
2024-06-21 14:53:53 -0300

    commit: feat: adicionei algumas funcionalidades de tentativas mostra quantas vezes usuario tentou jogar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+;
 const input= document.querySelector('.modal-input');
 
 const buttonModal = document.querySelector('.button-modal');
@@ -7,40 +8,50 @@ const buttonInit = document.querySelector('.button');
 const buttonClose = document.querySelector('.modal-close');
 
 const modal = document.querySelector('.modal');
-let numeroSecreto = 36
+const message = document.querySelector('p'); 
+let numeroSecreto =  Math.floor(Math.random() * 40) + 1
 
 function show () {
-
  modal.classList.add('ativo');
 }
 
 function Close () {
-    const modal = document.querySelector('.modal');
-    modal.classList.remove('ativo');
+  const modal = document.querySelector('.modal');
+  modal.classList.remove('ativo');
 }
 
 function game ( ) {
   const userInput = input.value;
-  const numero = parseInt(userInput)
+  const numero = parseInt(userInput);
   const message = document.querySelector('p'); 
-   
-  if(numeroSecreto === numero){
-    console.log('acertou')
-    message.style.fontSize = '1.9rem';
-    message.style.color = 'green'
-    message.textContent =  `Você acertou o numero secreto é ${numero} `;
-  }else {
-    console.log('errou')
-    message.textContent ='você errou tente novamente!'
-    message.classList.add('successo')
-    message.style.fontSize = '1.9rem';
-    message.style.color = 'red'
+  
+  // Inicializa o contador de tentativas se não existir
+  if (!game.tentativas) {
+    game.tentativas = 0; 
   }
- 
+  
+  game.tentativas++; // Incrementa o contador a cada tentativa
+  if(numeroSecreto === numero){
+    console.log('acertou');
+    message.style.fontSize = '1.9rem';
+    message.style.color = 'green';
+    message.textContent =  `Você acertou! O número secreto é ${numero}. tentativas: ${game.tentativas}`;
+  }else if (numero > numeroSecreto) {
+      message.style.fontSize = '1.9rem';
+      message.style.color = 'orange';
+      message.textContent = `Você está perto, o número é um pouco meno, com tentativas: ${game.tentativas}`;
+      modal.classList.remove('ativo');
+    }else {
+    console.log('errou');
+    message.textContent =`Você errou, tente novamente! Tentativas: ${game.tentativas}`;
+    message.classList.add('successo');
+    message.style.fontSize = '1.9rem';
+    message.style.color = 'red';
+  }
+  game.tentativas++;
   modal.classList.remove('ativo');
-
+  input.value = ''; // Limpa o campo de input após a tentativa
 }
-
 
 buttonModal.addEventListener('click', game)
 
