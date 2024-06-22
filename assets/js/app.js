2ed4ca2e0d7740667e43903bf02c361bb5749185
Lucasgarcc <luuccasgarciia@gmail.com>
2024-06-21 21:40:01 -0300

    commit: :feat: adicionei a funcionalidade de dica

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-;
 const input= document.querySelector('.modal-input');
 
 const buttonModal = document.querySelector('.button-modal');
@@ -8,8 +7,10 @@ const buttonInit = document.querySelector('.button');
 const buttonClose = document.querySelector('.modal-close');
 
 const modal = document.querySelector('.modal');
-const message = document.querySelector('p'); 
-let numeroSecreto =  Math.floor(Math.random() * 40) + 1
+
+let numeroSecreto =  5
+
+
 
 function show () {
  modal.classList.add('ativo');
@@ -24,31 +25,30 @@ function game ( ) {
   const userInput = input.value;
   const numero = parseInt(userInput);
   const message = document.querySelector('p'); 
-  
+  let tentativas = 0
   // Inicializa o contador de tentativas se não existir
   if (!game.tentativas) {
-    game.tentativas = 0; 
+   tentativas = game.tentativas = 0; 
   }
   
   game.tentativas++; // Incrementa o contador a cada tentativa
-  if(numeroSecreto === numero){
+  if(numeroSecreto === numero) {
     console.log('acertou');
     message.style.fontSize = '1.9rem';
     message.style.color = 'green';
     message.textContent =  `Você acertou! O número secreto é ${numero}. tentativas: ${game.tentativas}`;
-  }else if (numero > numeroSecreto) {
+  }else if (numero > numeroSecreto ) {
       message.style.fontSize = '1.9rem';
       message.style.color = 'orange';
-      message.textContent = `Você está perto, o número é um pouco meno, com tentativas: ${game.tentativas}`;
+      message.textContent = `Você está perto, o número é um pouco menor que ${numero}, com tentativas: ${game.tentativas} `;
       modal.classList.remove('ativo');
     }else {
     console.log('errou');
     message.textContent =`Você errou, tente novamente! Tentativas: ${game.tentativas}`;
-    message.classList.add('successo');
     message.style.fontSize = '1.9rem';
     message.style.color = 'red';
   }
-  game.tentativas++;
+  tentativas++;
   modal.classList.remove('ativo');
   input.value = ''; // Limpa o campo de input após a tentativa
 }
